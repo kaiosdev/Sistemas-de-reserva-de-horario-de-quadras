3134bd3447e5599948e52dc8ff13487685257826
Jean Carlos dos Santos Baraúna <jean.pc.gaster@gmail.com>
2026-05-22 17:26:51 -0400

    Update 05_Pagamento.sql

--- sql/05_Pagamento.sql
+++ sql/05_Pagamento.sql
@@ -1,1 +1,12 @@
+USE athletix_db;
 
+-- Cria a tabela financeira final do sistema
+CREATE TABLE Pagamento (
+    id_pagamento INT AUTO_INCREMENT PRIMARY KEY,
+    id_cliente INT NOT NULL,
+    valor DECIMAL(10, 2) NOT NULL,
+    data_pagamento DATE NOT NULL,
+    status_pagamento VARCHAR(20) DEFAULT 'Pendente',
+    -- Relacionamento com a tabela criada pelo Membro 1 (Construtor Base)
+    FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente)
+);
